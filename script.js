30fbc0556fe9d089893006db4c1be1f66b2682e3
Nikhilkale2 <nikhilkaledev@gmail.com>
2026-02-03 06:43:31 +0530

    create Navbar & Hero section and make it responsive across all devices

---
+++ script.js
@@ -0,0 +1,43 @@
+let mm = gsap.matchMedia();
+mm.add("(max-width:1024px)", () => {
+  let nav = gsap.timeline();
+
+  nav.to("#navLinks", {
+    left: 0,
+    duration: 0.8,
+  });
+  nav.from(".mobile-logo", {
+    x: -40,
+    opacity: 0,
+    duration: 0.8,
+  });
+  nav.from("#navLinks li", {
+    x: -60,
+    opacity: 0,
+    duration: 0.7,
+    stagger: 0.3,
+  });
+  nav.from(".sign-btn", {
+    x: -60,
+    opacity: 0,
+    duration: 0.8,
+  });
+  nav.from("#navClose", {
+    scale: 2,
+    duration: 0.9,
+    opacity: 0,
+  });
+
+  nav.pause();
+  let MenuIcon = document.querySelector("#MenuIcon");
+  let navClose = document.querySelector("#navClose");
+
+  MenuIcon.addEventListener("click", function () {
+    nav.play();
+  });
+  navClose.addEventListener("click", function () {
+    nav.reverse();
+
+    // Hero section
+  });
+});
